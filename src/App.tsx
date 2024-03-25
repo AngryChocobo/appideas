2b511a483d4a4eac3f5081163b478665502a9877
AngryChocobo <1347237169@qq.com>
2024-03-25 18:12:43 +0800

    feat: home page idea list and tier list

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,32 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
+import { Checkbox } from "@/components/ui/checkbox";
+import { ideaList } from "./ideaList";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+import { Progress } from "@/components/ui/progress";
 
 function App() {
   const [tierList] = useState(["Beginner", "Intermediate", "Advanced"]);
+  const [checkedTier] = useState(["Beginner", "Intermediate", "Advanced"]);
+  const progress =
+    (ideaList.filter((v) => v.status).length / ideaList.length) * 100;
+
   return (
-    <>
+    <div className="max-w-screen-md m-auto">
       <div className="flex">
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -16,14 +36,58 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <Button>Click me</Button>
-      <div>
+      <div className="flex items-center space-x-4 mb-4">
+        <div className="text-lg">Tier Options </div>
         {tierList.map((tier) => (
-          <span key={tier}>{tier}</span>
+          <div key={tier} className="flex items-center">
+            <Checkbox id={tier} checked={checkedTier.includes(tier)} />
+            <label
+              htmlFor={tier}
+              className="ml-1 text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+            >
+              {tier}
+            </label>
+          </div>
         ))}
       </div>
-    </>
+      <Table>
+        <TableCaption>A list of your recent invoices.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Name</TableHead>
+            <TableHead>Short Description</TableHead>
+            <TableHead>Tier</TableHead>
+            <TableHead className="text-right">Status</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {ideaList.map((idea) => (
+            <TableRow key={idea.href}>
+              <TableCell className="font-medium">
+                <Button variant="link" className="px-0 font-bold">
+                  <Link to={idea.href}>{idea.name}</Link>
+                </Button>
+              </TableCell>
+              <TableCell>{idea.shortDescription}</TableCell>
+              <TableCell>
+                <Badge key={idea.tier}>{idea.tier}</Badge>
+              </TableCell>
+              <TableCell className="text-right">
+                {idea.status ? "✅" : "⭕️"}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TableCell colSpan={2}>Total</TableCell>
+            <TableCell colSpan={2} className="text-right">
+              <Progress value={progress} />
+            </TableCell>
+          </TableRow>
+        </TableFooter>
+      </Table>
+    </div>
   );
 }
 
---
+++ src/ideaList.ts
@@ -0,0 +1,16 @@
+export const ideaList = [
+  {
+    name: "Bin2Dec",
+    shortDescription: "Binary-to-Decimal number converter",
+    status: true,
+    tier: "Beginner",
+    href: "bin2dec",
+  },
+  {
+    name: "Border Radius Previewer",
+    shortDescription: "Preview how CSS3 border-radius values affect an element",
+    status: false,
+    tier: "Beginner",
+    href: "border-radius-previewer",
+  },
+];
