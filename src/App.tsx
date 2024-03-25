2a429d9012a3f508fe84aec0b9cf58d8d14eb51e
AngryChocobo <1347237169@qq.com>
2024-03-25 16:26:02 +0800

    feat: add vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [tierList] = useState(["Beginner", "Intermediate", "Advanced"]);
   return (
     <>
-      <div>
+      <div className="flex">
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -14,6 +16,11 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <div>
+        {tierList.map((tier) => (
+          <span key={tier}>{tier}</span>
+        ))}
+      </div>
     </>
   );
 }
